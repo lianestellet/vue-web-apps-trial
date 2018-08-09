3812260d4ae3a88db1122368d9a5e5b8c7dae657
prsad <prsad@cesar.org>
2018-08-09 17:41:54 -0300

    Recovering data using store and states

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-      Got Jokes?
+      <h3>Got Jokes?</h3>
+      <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+      <div class="">
+
+      </div>
+
       {{$store.state}}
   </div>
 </template>
+
+<script>
+
+import { mapActions } from 'vuex'
+
+export default {
+  methods: mapActions([
+    'initJokes'
+  ])
+}
+</script>
+
