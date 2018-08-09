d8b6b6ded7160d3520f2829520e96d8091908d4e
prsad <prsad@cesar.org>
2018-08-09 10:35:04 -0300

    Routing on vue.js

---
+++ applications/starbase-router/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h3>Starbase Router</h3>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
